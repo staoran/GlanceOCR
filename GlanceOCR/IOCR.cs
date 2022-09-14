00bfc2ee742321ec11b68fa53d2fdb2f1c2101fb
taoran <staoran@gmail.com>
2022-09-14 22:32:12 +0800

    新增 OCR 识别项目，定义 IOCR 接口，PaddleOCR 实现类

---
+++ GlanceOCR/IOCR.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+
+namespace GlanceOCR;
+
+public interface IOCR : IDisposable
+{
+    /// <summary>
+    /// 检查服务是否可用
+    /// </summary>
+    /// <returns></returns>
+    Task<bool> Check();
+
+    /// <summary>
+    /// 文本识别
+    /// </summary>
+    /// <returns></returns>
+    Task<string> DetectText(Image image);
+}
