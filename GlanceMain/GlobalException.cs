eede868d04c8ab8fecff4b197eeb0a92a2828afa
taoran <staoran@gmail.com>
2022-09-25 00:34:55 +0800

    全局异常处理程序独立成单类

---
+++ GlanceMain/GlobalException.cs
@@ -0,0 +1,52 @@
+﻿namespace GlanceMain;
+
+/// <summary>
+/// 全局异常处理程序
+/// </summary>
+public static class GlobalException
+{
+    /// <summary>
+    /// 处理全局异常
+    /// </summary>
+    public static void Handle()
+    {
+        // 向事件中添加用于处理UI线程异常的事件处理程序
+        Application.ThreadException += Application_ThreadException;
+
+        // 设置未处理的异常模式以强制所有Windows窗体错误通过我们的处理程序
+        Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+
+        // 向事件中添加用于处理非UI线程异常的事件处理程序
+        AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+    }
+
+    /// <summary>
+    /// UI线程异常的事件处理程序
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    /// <exception cref="NotImplementedException"></exception>
+    private static void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
+    {
+        if(e.Exception is HotkeyAlreadyRegisteredException ex)
+            UIMessageTip.ShowError(MainForm.Current, $"快捷键 {ex.Name} 已经被其他程序注册");
+        else
+        {
+            UIMessageTip.ShowError(e.Exception.Message);
+        }
+    }
+
+    /// <summary>
+    /// 非UI线程异常的事件处理程序
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    /// <exception cref="NotImplementedException"></exception>
+    private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+    {
+        if (e.ExceptionObject is Exception ex)
+        {
+            UIMessageTip.ShowError(ex.Message);
+        }
+    }
+}
--- GlanceMain/Program.cs
+++ GlanceMain/Program.cs
@@ -15,18 +15,8 @@ static class Program
     [STAThread]
     static void Main()
     {
-        #region 全局异常事件
-
-        // 向事件中添加用于处理UI线程异常的事件处理程序
-        Application.ThreadException += Application_ThreadException;
-
-        // 设置未处理的异常模式以强制所有Windows窗体错误通过我们的处理程序
-        Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
-
-        // 向事件中添加用于处理非UI线程异常的事件处理程序
-        AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
-
-        #endregion
+        // 处理全局异常
+        GlobalException.Handle();
 
         // 多开处理
         RunningInstance.Handle();
@@ -41,40 +31,6 @@ static class Program
 
         Application.Run(MainForm.Current);
     }
-
-    #region 异常处理程序
-
-    /// <summary>
-    /// UI线程异常的事件处理程序
-    /// </summary>
-    /// <param name="sender"></param>
-    /// <param name="e"></param>
-    /// <exception cref="NotImplementedException"></exception>
-    private static void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
-    {
-        if(e.Exception is HotkeyAlreadyRegisteredException ex)
-            UIMessageTip.ShowError(_main, $"快捷键 {ex.Name} 已经被其他程序注册");
-        else
-        {
-            UIMessageTip.ShowError(e.Exception.Message);
-        }
-    }
-
-    /// <summary>
-    /// 非UI线程异常的事件处理程序
-    /// </summary>
-    /// <param name="sender"></param>
-    /// <param name="e"></param>
-    /// <exception cref="NotImplementedException"></exception>
-    private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
-    {
-        if (e.ExceptionObject is Exception ex)
-        {
-            UIMessageTip.ShowError(ex.Message);
-        }
-    }
-
-    #endregion
 }
 
 public class Startup : AppStartup
